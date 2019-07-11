d9a18420efeefbff8862cefd73e4caf64e19693d
Ivo Ventura <ivo.ventura@globalhitss.com.br>
2019-07-11 10:21:02 -0300

    Primeiros estudos em ruby

---
+++ operadores.rb
@@ -0,0 +1,13 @@
+numero1 = 10
+numer2 = 11
+
+# Operadores de comparação...
+
+# maior que >
+# menor que <
+# maior ou igual >=
+# menor ou igual <=
+# diferente !=
+# equals = eql?
+
+puts numero1.eql?(numer2)
