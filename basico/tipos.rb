5c1c14116cdf37f0f405f869e2c3749e7bd97cb2
flavianventura <flavianventura@gmail.com>
2019-07-13 15:00:36 -0300

    Alterando a estrutura de pastas do projeto

---
+++ basico/tipos.rb
@@ -0,0 +1,15 @@
+produto = 'macbook air'
+preco = 5.000
+quantidade = 10
+disponivel = true
+
+puts produto
+puts preco
+puts quantidade
+puts disponivel
+
+# O .class serve para resgatar o tipo de dado atribuído às variáveis
+puts produto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class
