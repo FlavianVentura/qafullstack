a50d7606bbd89bdcc7ac667b7b7615d6daf9a256
flavianventura <flavianventura@gmail.com>
2019-07-17 11:25:06 -0300

    Iniciando regras de negocios com BDD em Ruby

---
+++ unitarios/spec/avengers_spec.rb
@@ -0,0 +1,27 @@
+class AvengersHeadQuater
+
+    attr_accessor :list
+
+    def initialize
+
+        self.list = []
+    end
+
+    def put(avenger)
+
+        self.list.push(avenger)
+    end
+
+end
+
+describe AvengersHeadQuater do
+
+    it 'Deve adicionar um novo vingador' do
+
+        hq = AvengersHeadQuater.new
+        hq.put('Spiderman')
+
+        expect(hq.list).to eql ['Spiderman']
+        
+    end
+end
